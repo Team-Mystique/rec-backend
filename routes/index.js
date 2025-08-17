d999dc84e9fb9e122fab026f2b59dc8b0331dbd3
RoselynFrancis <rfrancis@stu.ug.edu.gh>
2025-08-17 19:10:34 +0000

    user registration and login routes added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { home } = require('../controllers/indexController');
+const { login, register } = require('../controllers/userController');
 
 // Define a route for the home page
 router.get('/', home); 
+
+// Login route
+router.post('/login', login);
+router.post('/register', register)
 
 module.exports = router;
 
